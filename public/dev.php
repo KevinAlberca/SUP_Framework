69ee2f1736a3ed79bf5fc751c20da7c4aadf64ae
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-01-20 15:43:26 +0100

    Router is available

--- public/dev.php
+++ public/dev.php
@@ -1,21 +1,16 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: AwH
  * Date: 20/01/16
  * Time: 09:41
  */
+
 require_once("../vendor/autoload.php");
 
 $router = new Core\Router("dev");
 
 if($router->checkIfRouteExist($router->getRoute())){
-    // Require the view
-    echo "<h1>View</h1>";
-} else {
-    // Require the 404 error page
-    echo "<h1>404</h1>";
+    // Require the View
 }
-
-
-echo "ok";
