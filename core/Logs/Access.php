e9e2f4a5bada0a5b754b695fa4975d8145e7806c
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-01-27 12:46:59 +0100

    #2 Access : get UserAgent

--- core/Logs/Access.php
+++ core/Logs/Access.php
@@ -13,10 +13,15 @@ class Access
 {
 
     public function __construct() {
+        var_dump($this->getTime(), $this->getUserAgent());
     }
 
     private function getTime() {
-        return date("Y-m-d H:i:s");
+        return "[".date("Y-m-d H:i:s")."]";
+    }
+
+    private function getUserAgent() {
+        return $_SERVER["HTTP_USER_AGENT"];
     }
 
 }
