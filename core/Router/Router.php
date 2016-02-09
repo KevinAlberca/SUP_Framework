9c344c5ee0ae8ad5958810004715358577cdb75f
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-02-09 10:12:38 +0100

    fix bug routing

--- core/Router/Router.php
+++ core/Router/Router.php
@@ -96,7 +96,7 @@ class Router
                         "controller" => $routes["controller"],
                         "arguments" => $arguments,
                     ];
-                } 
+                }
             }
         }
     }
